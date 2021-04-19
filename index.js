b2702c389055b514b0da9f84e5d944fe98fba0c8
robbycorrs23 <82347372+robbycorrs23@users.noreply.github.com>
2021-04-18 20:14:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const firstName = document.querySelector("#inputFirst")
+const lastName = document.querySelector("#inputLast")
+const firstBtn = document.querySelector("#subFirst")
+const lastBtn = document.querySelector("#subLast")
+const lastShow = document.querySelector("#lastDisplay")
 
+function grabInput () {
+    let firstShow = document.getElementById("firstDisplay")
+    firstShow.innerText = firstName.value;
+}
+firstBtn.addEventListener("click", grabInput)
+
+let entryDiv = document.createElement("div");
+entryDiv.className = "single-entry";
+entryDiv.textContent = "This is an entry Div, see me roar.";
+body.append(entryDiv)
